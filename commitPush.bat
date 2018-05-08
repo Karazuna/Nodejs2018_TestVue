df0af583622d82ccf1c0a07a9585f04356fabac5
Karazuna <kchy12345@gmail.com>
2018-05-08 12:00:55 +0900

    2018-05-08, VueJS - test index.html; Bulma CSS test + document link

--- commitPush.bat
+++ commitPush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "2018-05-08, VueJS - test index.html; Bulma CSS test"
+git commit -m "2018-05-08, VueJS - test index.html; Bulma CSS test + document link"
 git push -u origin master
